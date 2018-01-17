716af5607c2fb6e9ee631692189262212ea7b496
scodella <luca.scodellaro@cern.ch>
2018-01-17 14:08:23 +0100

    merge stop

--- stop/merge.sh
+++ stop/merge.sh
@@ -16,15 +16,18 @@ pushd $FOLDER
 hadd -f -k 01_Data.root       *03Feb2017*
 ##hadd -f -k 01_DataBlind.root   *Run2016B-03Feb2017* *Run2016C-03Feb2017* *Run2016D-03Feb2017* 
 hadd -f -k 02_WZTo3LNu.root  WZTo3LNu.root
-hadd -f -k 03_VZ.root        ZZTo2L2Q__part*.root ZZTo2L2Nu__part*.root WZTo2L2Q__part*.root
+##hadd -f -k 03_VZ.root        ZZTo2L2Q__part*.root ZZTo2L2Nu__part*.root WZTo2L2Q__part*.root
+hadd -f -k 03_ZZ.root        ZZTo2L2Nu__part*.root ggZZTo2e2nu.root ggZZTo2mu2nu.root
+##hadd -f -k 03a_ZZ.root        ZZTo2L2Nu__part*.root 
+##hadd -f -k 03b_ZZ.root        ggZZTo2e2nu.root ggZZTo2mu2nu.root
 hadd -f -k 04_TTTo2L2Nu.root TTTo2L2Nu__part*.root
 hadd -f -k 05_ST.root        ST_tW_antitop.root ST_tW_top.root
 hadd -f -k 06_WW.root        WWTo2L2Nu.root GluGluWWTo2L2Nu_MCFM.root
-hadd -f -k 07_ZJets.root     DYJetsToLL_M-10to50.root DYJetsToLL_M-50__part*.root
-#hadd -f -k 07_ZJetsHT_DYcorr.root   DYJetsToLL_M-10to50-LO_DYcorr.root DYJetsToLL_M-5to50_HT-*_DYcorr.root 
-DYJetsToLL_M-50-LO-ext1__part*_DYcorr.root DYJetsToLL_M-50_HT-70to100__part*_DYcorr.root DYJetsToLL_M-50_HT-100to200_ext1__part*_DYcorr.root DYJetsToLL_M-50_HT-200to400_ext1__part*_DYcorr.root DYJetsToLL_M-50_HT-400to600_DYcorr.root DYJetsToLL_M-50_HT-600to800__part*_DYcorr.root DYJetsToLL_M-50_HT-800to1200_DYcorr.root DYJetsToLL_M-50_HT-1200to2500_DYcorr.root DYJetsToLL_M-50_HT-2500toInf_DYcorr.root
-#rm DY*_DYcorr.root
-hadd -f -k 07_ZJetsHT.root   DYJetsToLL_M-10to50-LO.root DYJetsToLL_M-5to50_HT-*root DYJetsToLL_M-50-LO-ext1__part*.root DYJetsToLL_M-50_HT-70to100__part* DYJetsToLL_M-50_HT-100to200_ext1__part* DYJetsToLL_M-50_HT-200to400_ext1__part* DYJetsToLL_M-50_HT-400to600.root DYJetsToLL_M-50_HT-600to800__part* DYJetsToLL_M-50_HT-800to1200.root DYJetsToLL_M-50_HT-1200to2500.root DYJetsToLL_M-50_HT-2500toInf.root  
+#hadd -f -k 07_ZJets.root     DYJetsToLL_M-10to50.root DYJetsToLL_M-50__part*.root
+hadd -f -k 07_ZJetsHT_DYcorr.root   DYJetsToLL_M-10to50-LO_DYcorr.root DYJetsToLL_M-5to50_HT-*_DYcorr.root DYJetsToLL_M-50-LO-ext1__part*_DYcorr.root DYJetsToLL_M-50_HT-70to100__part*_DYcorr.root DYJetsToLL_M-50_HT-100to200_ext1__part*_DYcorr.root DYJetsToLL_M-50_HT-200to400_ext1__part*_DYcorr.root DYJetsToLL_M-50_HT-400to600_DYcorr.root DYJetsToLL_M-50_HT-600to800__part*_DYcorr.root DYJetsToLL_M-50_HT-800to1200_DYcorr.root DYJetsToLL_M-50_HT-1200to2500_DYcorr.root DYJetsToLL_M-50_HT-2500toInf_DYcorr.root
+##rm DY*_DYcorr.root
+#hadd -f -k 07_ZJetsHT.root   DYJetsToLL_M-10to50-LO.root DYJetsToLL_M-5to50_HT-*root DYJetsToLL_M-50-LO-ext1__part*.root 
+DYJetsToLL_M-50_HT-70to100__part* DYJetsToLL_M-50_HT-100to200_ext1__part* DYJetsToLL_M-50_HT-200to400_ext1__part* DYJetsToLL_M-50_HT-400to600.root DYJetsToLL_M-50_HT-600to800__part* DYJetsToLL_M-50_HT-800to1200.root DYJetsToLL_M-50_HT-1200to2500.root DYJetsToLL_M-50_HT-2500toInf.root  
 ##hadd -f -k 08_WJets.root     WJetsToLNu.root
 hadd -f -k 09_TTW.root       TTWJetsToLNu.root TTWJetsToQQ.root 
 hadd -f -k 10_TTZ.root       TTZToQQ.root TTZToLLNuNu_M-10.root 
@@ -34,6 +37,11 @@ hadd -f -k 11_HWW.root       GluGluHToWWTo2L2NuAMCNLO_M125.root VBFHToWWTo2L2Nu_
 hadd -f -k 13_VVV.root       WWW.root WWZ.root WZZ.root #ZZZ.root
 ##hadd -f -k 14_HZ.root        HZJ_HToWWTo2L2Nu_M125_noHLT.root GluGluZH_HToWWTo2L2Nu_M125_noHLT.root
 ##hadd -f -k 15_WgStar.root    WgStarLNuEE.root WgStarLNuMuMu.root
-hadd -f -k 14_ZZTo4L.root        ZZTo4L__part*.root
+#hadd -f -k 14_ZZTo4L.root        ZZTo4L__part*.root ggZZTo4mu.root ggZZTo4e.root ggZZTo4tau.root ggZZTo2e2mu.root ggZZTo2mu2tau.root ggZZTo2e2tau.root qqHToZZTo4L.root ggHToZZTo4L.root
+#hadd -f -k 14a_ZZTo4L.root        ZZTo4L__part*.root 
+#hadd -f -k 14b_ZZTo4L.root        ggZZTo4mu.root ggZZTo4e.root ggZZTo4tau.root ggZZTo2e2mu.root ggZZTo2mu2tau.root ggZZTo2e2tau.root 
+#hadd -f -k 14c_ZZTo4L.root        qqHToZZTo4L.root ggHToZZTo4L.root
+hadd -f -k 15_VZ.root        WZTo2L2Q__part*.root ZZTo2L2Q__part*.root 
+hadd -f -k 15_VZ3V.root       15_VZ.root 13_VVV.root 
 
 popd
